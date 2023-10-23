14386d2cd919172c8c739564354423c1c8ff7826
Andrew Mead <andrewmead@verizon.net>
2023-10-22 23:21:55 -0400

    add tweet generation

--- test_tables.py
+++ test_tables.py
@@ -1,11 +1,17 @@
 import sqlite3
+
 con = sqlite3.connect("twitter.db")
 cur = con.cursor()
 
-#read the contents of the databases 
+# read the contents of the databases
 
 trends = cur.execute("SELECT * FROM trends").fetchall()
 tweets = cur.execute("SELECT * FROM tweets").fetchall()
 
 # print(trends)
-print(tweets)
+print(tweets[0])
+
+
+# print the columns of tweets
+for column in cur.execute("PRAGMA table_info(tweets)").fetchall():
+    print(column[1])
